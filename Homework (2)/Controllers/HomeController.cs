3960d15d61bba2c83d98786cfbb1ae3fb7a3d1fe
ZRavenscroft <110469428+ZRavenscroft@users.noreply.github.com>
2022-11-17 01:06:16 +0400

    Homework

---
+++ Homework (2)/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework__2_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+    }
+}
